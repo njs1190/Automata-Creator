aacd9c09165baf4ca3d61ed29a4c0d415d89402a
jillianjakubowicz <jakubowiczj@merrimack.edu>
2014-04-10 17:42:26 -0400

    Update myCanvas.java

--- src/myCanvas.java
+++ src/myCanvas.java
@@ -9,17 +9,23 @@ import java.awt.event.MouseListener;
 import javax.swing.JScrollPane;
 import javax.swing.JPanel;
 
+import java.util.ArrayList;
+
 class myCanvas extends JPanel implements MouseListener
 {
     Image img;      // Contains the image to draw on MyCanvas
+    ArrayList<State> _states; // The list of states that have been added to the canvas
+    ArrayList<Transition> _transitions; // The list of transitions that have been added to the canvas
     
     public myCanvas()
-    {
-    	
+    {    	
+    	_states = new ArrayList<State>();
+    	_transitions = new ArrayList<Transition>();    	
     }
 
     public void AddCanvas(JScrollPane canvasPanel)
-    {
+    {  	
+    
     	Canvas automataCanvas = new Canvas();
     	automataCanvas.setSize(700, 320);
     	automataCanvas.setBackground(Color.WHITE);
@@ -57,5 +63,4 @@ class myCanvas extends JPanel implements MouseListener
 		// TODO Auto-generated method stub
 		
 	}
-
 }
