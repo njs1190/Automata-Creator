9e76ec1c770940d1ea7e57ed2f972632ea1ab601
njs1190 <shahinn@merrimack.edu>
2014-04-15 00:19:34 -0400

    Update outPanel.java

--- src/outPanel.java
+++ src/outPanel.java
@@ -13,73 +13,98 @@ import javax.swing.border.EmptyBorder;
 public class outPanel 
 {	
 	static JTextArea outputTextArea;
-	
+
 	public outPanel()
 	{
-		
+
 	}	
-	
+
 	public void BuildOutputPanel (JPanel outputPanel) 
 	{
         
-		 // Initiate Feedback/Error Window
+		
+		
+		
+		// Initiate Feedback/Error Window
          outputTextArea = new JTextArea(5, 15);
          outputTextArea.setEditable(false);
          outputTextArea.setBorder(BorderFactory.createLineBorder(Color.black));
-    
-	     outputPanel.add(outputTextArea, BorderLayout.LINE_START);
+         
+        JPanel outTextAreaPanel = new JPanel(new GridLayout(1,1));
+         
+         
+        outTextAreaPanel.add(outputTextArea);
+        outputPanel.add(outTextAreaPanel, BorderLayout.WEST);
+        outputPanel.setBorder(BorderFactory.createEmptyBorder(10,110,10,0));
 	     
-	     JPanel stringProcessPanel = new JPanel (new GridLayout(2,3,20,0));
 	     
+	     
+	     
+	     
+	     JPanel stringProcessPanel = new JPanel (new GridLayout(1,3,20,0));
+
 	     //Initiate text area for already processed symbols,the current symbol, and the symbols still on the tape
 	     JTextArea processedSymbols = new JTextArea(1,6);
 	     JTextArea tapeSymbols = new JTextArea(1,6);
+	     JTextArea currentSymbol = new JTextArea();
+	     currentSymbol.setSize(5, 6);
+	     
+	     
+	     
+	     
+	     
 	     // make them not editable 
 	     processedSymbols.setEditable(false);
-	     
 	     tapeSymbols.setEditable(false);
-	     
-	     JPanel currentPanel = new JPanel(new GridLayout(1,3));
-	     JTextArea currentSymbol = new JTextArea(1,1);
 	     currentSymbol.setEditable(false);
+	   
 	     
-	     JPanel currentSpacer = new JPanel();
-	     currentSpacer.setBorder(new EmptyBorder(10, 10, 10, 7) );
-	     JPanel currentSpacer2 = new JPanel();
-	     currentSpacer.setBorder(new EmptyBorder(10, 10, 10, 7) );
-	    
-	     currentPanel.add(currentSpacer);
-	     currentPanel.add(currentSymbol);
-	     currentPanel.add(currentSpacer2);
+	     //add labels for text areas
+	     JLabel processedLabel = new JLabel("Processed Symbols");
+	     JLabel currentLabel = new JLabel("<html>   Current<br/>Symbol</html> ");
+	     JLabel tapeLabel = new JLabel(" Symbols on Tape");
+	 
 	     
-	    
-	     currentSymbol.setSize(6,5);
+	     
+	     
 	     //add Borders to string tracker text areas
 	     processedSymbols.setBorder(BorderFactory.createLineBorder(Color.black));
 	     currentSymbol.setBorder(BorderFactory.createLineBorder(Color.black));
 	     tapeSymbols.setBorder(BorderFactory.createLineBorder(Color.black));
-	     //add labels for text areas
-	     JLabel processedLabel = new JLabel("Processed Symbols");
-	     JLabel currentLabel = new JLabel(" Current Symbol");
-	     JLabel tapeLabel = new JLabel(" Symbols on Tape");
-	     
-	     
-	     //  padding space between text boxes
-	     JPanel spacer = new JPanel();
-	     spacer.setBorder(new EmptyBorder(10, 10, 10, 75) );
-	 
-	     // add labels and textboxes to stringProcessPanel
-	     stringProcessPanel.add(processedSymbols);
-	     stringProcessPanel.add(currentPanel);
-	     stringProcessPanel.add(tapeSymbols);
-	     stringProcessPanel.add(processedLabel);
-	     stringProcessPanel.add(currentLabel);
-	     stringProcessPanel.add(tapeLabel);
-	     
-	
-	    outputPanel.add(spacer);
-	    outputPanel.add(stringProcessPanel, BorderLayout.EAST);
 	    
+	     
+	    
+	    
+	     JPanel currentSymbolPanel = new JPanel(new GridLayout(2,1));
+	     currentSymbolPanel.add(currentSymbol);
+	     currentSymbolPanel.add(currentLabel);
+	     currentSymbolPanel.setBorder(BorderFactory.createEmptyBorder(0,30,0,30));
+	     
+	    
+	     
+	     JPanel processedSymbolsPanel = new JPanel(new GridLayout(2,1));
+	     processedSymbolsPanel.add(processedSymbols);
+	     processedSymbolsPanel.add(processedLabel);
+	     
+	     JPanel tapeSymbolsPanel = new JPanel(new GridLayout(2,1));
+	     tapeSymbolsPanel.add(tapeSymbols);
+	     tapeSymbolsPanel.add(tapeLabel);
+	     
+	     
+	     
+	     // add labels and textboxes to stringProcessPanel
+	    
+	     stringProcessPanel.add(processedSymbolsPanel);
+	     stringProcessPanel.add(currentSymbolPanel);
+	     stringProcessPanel.add(tapeSymbolsPanel);
+	     
+	     
+	     
+	     stringProcessPanel.setBorder(BorderFactory.createEmptyBorder(25,20,0,30));
+
+	    
+	    outputPanel.add(stringProcessPanel, BorderLayout.EAST);
+
     }
-	
+
 }
