5f363f2733d39cab2f7db4fb14ced880c307c52e
jillianjakubowicz <jakubowiczj@merrimack.edu>
2014-04-22 14:27:58 -0400

    Create DrawableObject.java

---
+++ src/DrawableObject.java
@@ -0,0 +1,16 @@
+package automataCreator;
+
+import java.awt.Graphics;
+
+public class DrawableObject 
+{
+	public DrawableObject()
+	{
+		
+	}
+	
+    public void Draw(Graphics g)
+	{
+		// Will be implemented in inherited methods 
+	}
+}
