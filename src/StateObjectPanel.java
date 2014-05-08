19b79b0651f07ceafa698c0de382c6cd56bf8ca8
jillianjakubowicz <jakubowiczj@merrimack.edu>
2014-05-08 00:15:08 -0400

    Update StateObjectPanel.java

--- src/StateObjectPanel.java
+++ src/StateObjectPanel.java
@@ -1,8 +1,9 @@
+// StateObjectPanel.java
 // Author: Jidaeno
-// Class: StateObjectPanel.java
-// Purpose: Displays the state object buttons on the left side of the main window.
-// If they are clicked a state of that type will be added to the canvas that can
-// then be dragged about the canvas
+// Course: CSC4910
+// Description: This class is used to create the panel that is docked to the
+// left-hand side of the MainFrame. The panel provides buttons that can be clicked
+// to add states to the canvas. 
 
 package automataCreator;
 
@@ -15,7 +16,8 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class StateObjectPanel extends JPanel
-{
+{		
+	private MainFrame _mainFrame;
 	
 	// State object panel items
     private JPanel _stateObjectPanel;
@@ -24,9 +26,9 @@ public class StateObjectPanel extends JPanel
 	private JButton _intermediate;
 	private JButton _accept;
 	
-	public StateObjectPanel()
+	public StateObjectPanel(MainFrame mainFrame)
 	{
-		
+		_mainFrame = mainFrame;
 	}
 	
 	public void initializeStateObjectPanel()
@@ -36,33 +38,32 @@ public class StateObjectPanel extends JPanel
     	// Creates 4 buttons for the state panel
         _start = new JButton();
         // Start State icon
-        ImageIcon startIcon = new ImageIcon("Images/start.gif");
+        ImageIcon startIcon = new ImageIcon("start.gif");
         _start.setIcon(startIcon);
+        _start.setToolTipText("Add start state to the canvas");
         _start.setFocusable(false); // Turn off highlighting after user click
         _start.addActionListener(new ActionListener()
         {
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				StateEvent event = new StateEvent(this, Data.StateType.START);
-	        	StateEvents.sendStateEvent(event);
-				
+				_mainFrame.createState(Data.StateType.START);				
 			}
         	
         });
         
     	_startA = new JButton();
     	// Start accept Icon
-        ImageIcon startAIcon = new ImageIcon("Images/startA.gif");
+        ImageIcon startAIcon = new ImageIcon("startA.gif");
         _startA.setIcon(startAIcon);
+        _startA.setToolTipText("Add start accept state to the canvas");
         _startA.setFocusable(false); // Turn off highlighting after user click
         _startA.addActionListener(new ActionListener()
         {
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				StateEvent event = new StateEvent(this, Data.StateType.STARTACCEPT);
-	        	StateEvents.sendStateEvent(event);
+				_mainFrame.createState(Data.StateType.STARTACCEPT);
 				
 			}
         	
@@ -70,33 +71,32 @@ public class StateObjectPanel extends JPanel
         
     	_intermediate = new JButton();
     	// Intermediate state Icon
-        ImageIcon intermediateIcon = new ImageIcon("Images/intermediate.gif");
+        ImageIcon intermediateIcon = new ImageIcon("intermediate.gif");
         _intermediate.setIcon(intermediateIcon);
+        _intermediate.setToolTipText("Add intermediate state to the canvas");
         _intermediate.setFocusable(false); // Turn off highlighting after user click
         _intermediate.addActionListener(new ActionListener()
         {
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				StateEvent event = new StateEvent(this, Data.StateType.INTERMEDIATE);
-	        	StateEvents.sendStateEvent(event);
-				
+				_mainFrame.createState(Data.StateType.INTERMEDIATE);				
 			}
         	
         });
     	
     	_accept = new JButton();
     	// Accept Icon
-        ImageIcon acceptIcon = new ImageIcon("Images/accept.gif");
+        ImageIcon acceptIcon = new ImageIcon("accept.gif");
         _accept.setIcon(acceptIcon);
+        _accept.setToolTipText("Add final state to the canvas");
         _accept.setFocusable(false); // Turn off highlighting after user click
         _accept.addActionListener(new ActionListener()
         {
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				StateEvent event = new StateEvent(this, Data.StateType.ACCEPT);
-	        	StateEvents.sendStateEvent(event);
+				_mainFrame.createState(Data.StateType.ACCEPT);
 				
 			}
         	
@@ -106,8 +106,7 @@ public class StateObjectPanel extends JPanel
     	this.add(_startA);
     	this.add(_intermediate);
     	this.add(_accept);
-	
 		
 	}
-
+	
 }
