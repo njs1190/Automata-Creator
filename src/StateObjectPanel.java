eab574bdb0513af7fa8a9f6bd42df818e7b98907
zephirith <kimd@merrimack.edu>
2014-05-09 01:28:55 -0400

    Update StateObjectPanel.java

--- src/StateObjectPanel.java
+++ src/StateObjectPanel.java
@@ -38,7 +38,7 @@ public class StateObjectPanel extends JPanel
     	// Creates 4 buttons for the state panel
         _start = new JButton();
         // Start State icon
-        ImageIcon startIcon = new ImageIcon("start.gif");
+        ImageIcon startIcon = new ImageIcon(getClass().getResource("start.gif"));
         _start.setIcon(startIcon);
         _start.setToolTipText("Add start state to the canvas");
         _start.setFocusable(false); // Turn off highlighting after user click
@@ -54,7 +54,7 @@ public class StateObjectPanel extends JPanel
         
     	_startA = new JButton();
     	// Start accept Icon
-        ImageIcon startAIcon = new ImageIcon("startA.gif");
+        ImageIcon startAIcon = new ImageIcon(getClass().getResource("startA.gif"));
         _startA.setIcon(startAIcon);
         _startA.setToolTipText("Add start accept state to the canvas");
         _startA.setFocusable(false); // Turn off highlighting after user click
@@ -71,7 +71,7 @@ public class StateObjectPanel extends JPanel
         
     	_intermediate = new JButton();
     	// Intermediate state Icon
-        ImageIcon intermediateIcon = new ImageIcon("intermediate.gif");
+        ImageIcon intermediateIcon = new ImageIcon(getClass().getResource("intermediate.gif"));
         _intermediate.setIcon(intermediateIcon);
         _intermediate.setToolTipText("Add intermediate state to the canvas");
         _intermediate.setFocusable(false); // Turn off highlighting after user click
@@ -87,7 +87,7 @@ public class StateObjectPanel extends JPanel
     	
     	_accept = new JButton();
     	// Accept Icon
-        ImageIcon acceptIcon = new ImageIcon("accept.gif");
+        ImageIcon acceptIcon = new ImageIcon(getClass().getResource("accept.gif"));
         _accept.setIcon(acceptIcon);
         _accept.setToolTipText("Add final state to the canvas");
         _accept.setFocusable(false); // Turn off highlighting after user click
@@ -107,6 +107,14 @@ public class StateObjectPanel extends JPanel
     	this.add(_intermediate);
     	this.add(_accept);
 		
+	}
+	
+	// PRE: A change is made to the canvas
+	// POST: The buttons are either disbaled or enabled 
+	public void enableStartStates(boolean enable)
+	{
+		_start.setEnabled(enable);
+		_startA.setEnabled(enable);
 	}
 	
 }
